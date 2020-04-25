8e9cb9ab9bc2b6faed61bf2e5028a8f40084aaaf
Justin Breneman <justin.breneman@yahoo.com>
2020-04-25 03:36:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,10 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  batch_badge = []
+  attendees.each do |name|
+    batch_badge << badge_maker(name)
+  end
+end
